a0f60bd792bcff0d63d7749b7f5ed90bdff88d33
KubacoSK <jakublife123@gmail.com>
2023-10-30 15:45:28 +0100

    Added loading screen

--- LevelLoader.cs
+++ LevelLoader.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LevelLoader : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Slider slider;
+    public TextMeshProUGUI progressText;
+
     public void LoadLevel(int sceneIndex)
     {
         StartCoroutine(LoadAsynchoronously(sceneIndex));
@@ -13,9 +19,14 @@ public class LevelLoader : MonoBehaviour
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
 
+        loadingScreen.SetActive(true);
+
         while (!operation.isDone)
         {
-            Debug.Log(operation.progress);
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
 
             yield return null;
         }
