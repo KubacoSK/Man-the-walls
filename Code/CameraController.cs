3c4164cae0e03f6c9fdbf2bcb4efa0fbde99af4a
KubacoSK <jakublife123@gmail.com>
2023-12-02 14:21:23 +0100

    Added camera WASD movement

--- Code/CameraController.cs
+++ Code/CameraController.cs
@@ -1,21 +1,59 @@
 using UnityEngine;
-using Cinemachine;
+using static UnityEditor.PlayerSettings;
 
 public class CameraController : MonoBehaviour
 {
-    public CinemachineFreeLook freeLookCamera;
+    private float panSpeed = 10f;
+    private float panBorderThickness = 10f;
+    public Vector2 panLimit;
+    public Vector2 panMinimum;
+    public float minOrthographicSize = 2f;
+    public float maxOrthographicSize = 10f;
+    public float zoomSmoothness = 5f;
+    private float targetOrthographicSize;
 
-    void Update()
+    public float scrollSpeed;
+
+
+    private void Update()
     {
-        // Pan with arrow keys or WASD
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        CameraMovement();
+        CameraZoom();
+    }
 
-        freeLookCamera.m_XAxis.Value += horizontal;
-        freeLookCamera.m_YAxis.Value += vertical;
+    private void CameraMovement()
+    {
+        Vector3 Pos = transform.position;
 
-        // Zoom with the mouse wheel
-        float zoom = Input.GetAxis("Mouse ScrollWheel");
-        freeLookCamera.m_Orbits[0].m_Radius += zoom * 5f; // Adjust the multiplier as needed
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            Pos.y += panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            Pos.y -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            Pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            Pos.x += panSpeed * Time.deltaTime;
+        }
+
+
+        Pos.x = Mathf.Clamp(Pos.x, panMinimum.x, panLimit.x);
+        Pos.y = Mathf.Clamp(Pos.y, panMinimum.y, panLimit.y);
+        transform.position = Pos;
+
+    }
+    private void CameraZoom()
+    {
+
+        Vector3 Pos = transform.position;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        targetOrthographicSize = Mathf.Clamp(Camera.main.orthographicSize - scroll * scrollSpeed * 100f * Time.deltaTime, minOrthographicSize, maxOrthographicSize);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, targetOrthographicSize, Time.deltaTime * zoomSmoothness);
     }
 }
