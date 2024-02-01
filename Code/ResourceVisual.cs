0ee93d0aab222bebce3d0832db47ef512d96db9c
KubacoSK <jakublife123@gmail.com>
2024-02-01 20:30:57 +0100

    added resource visual

--- Code/ResourceVisual.cs
+++ Code/ResourceVisual.cs
@@ -6,17 +6,31 @@ using UnityEngine;
 
 public class ResourceVisual : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI CoalCount;
-    [SerializeField] private TextMeshProUGUI RedCrysCount;
-    [SerializeField] private TextMeshProUGUI BlueCrysCount;
+    [SerializeField] private TextMeshProUGUI CoalCountText;
+    [SerializeField] private TextMeshProUGUI RedCrysCountText;
+    [SerializeField] private TextMeshProUGUI BlueCrysCountText;
 
     [SerializeField] private TextMeshProUGUI CoalIncomeText;
     [SerializeField] private TextMeshProUGUI RedCIncomeText;
     [SerializeField] private TextMeshProUGUI BluCIncomeText;
+
+    private int CoalCount;
+    private int RedCryCount;
+    private int BlueCryCount;
+
+    private int CoalIncome;
+    private int RedCIncome;
+    private int BluIncome;
     void Start()
     {
         Zone.ZoneControlChanged += Zone_ZoneControlChanged;
         TurnSystem.Instance.OnTurnChanged += TurnSystem_OnTurnChanged;
+        foreach(Zone zone in ZoneManager.ReturnAlliedZones())
+        {
+            CoalIncome += zone.NumberOfCoal;
+            RedCIncome += zone.NumberOFRedCrystal;
+            BluIncome += zone.NumberOfBlueCrystal;
+        }
     }
 
     void Update()
@@ -26,16 +40,32 @@ public class ResourceVisual : MonoBehaviour
 
     private void Zone_ZoneControlChanged(object sender, EventArgs e)
     {
-        
+        Zone zone = sender as Zone;
+        UpdateResources(zone);
     }
 
     private void TurnSystem_OnTurnChanged(object sender, EventArgs e)
     {
-
+        
     }
 
     private void UpdateResources(Zone zone)
     {
+        if (zone.IsUnderAllycont())
+        {
+            CoalIncome += zone.NumberOfCoal;
+            RedCIncome += zone.NumberOFRedCrystal;
+            BluIncome += zone.NumberOfBlueCrystal;
+        }
+        else
+        {
+            CoalIncome -= zone.NumberOfCoal;
+            RedCIncome -= zone.NumberOFRedCrystal;
+            BluIncome -= zone.NumberOfBlueCrystal;
+        }
+        CoalIncomeText.text = "+" + CoalIncome;
+        RedCIncomeText.text = "+" + RedCIncome;
+        BluCIncomeText.text = "+" + BluIncome;
     }
 
     private void AddResources()
