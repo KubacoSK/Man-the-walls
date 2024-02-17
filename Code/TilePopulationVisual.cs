7e3dd8e1134503553e043bf2b7a4e13bfae84428
KubacoSK <jakublife123@gmail.com>
2024-02-17 21:27:47 +0100

    added zone population visual

--- Code/TilePopulationVisual.cs
+++ Code/TilePopulationVisual.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TilePopulationVisual : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Zone thisZone;
+    [SerializeField] private TextMeshProUGUI thisTilePopulationVisual;
+
+    private void Awake()
+    {
+        thisTilePopulationVisual.transform.position = thisZone.transform.position - new Vector3(0, (thisZone.GetZoneSizeModifier().y / 2) - 0.5f, 0);
+        
+    }
     void Start()
+    {
+        thisTilePopulationVisual.text = thisZone.GetNumberOfCitizens().ToString();
+        TurnSystem.Instance.OnTurnChanged += TilePopulationVisual_OnTurnChanged;
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateTilePopText()
     {
-        
+        thisTilePopulationVisual.text = thisZone.GetNumberOfCitizens().ToString("F2");
+        thisTilePopulationVisual.text
+    }
+
+    private void TilePopulationVisual_OnTurnChanged(object sender, EventArgs e)
+    {
+        UpdateTilePopText();
     }
 }
