7701cf6b2b1ef4b2383c92723b5fd25aa1789648
KubacoSK <jakublife123@gmail.com>
2024-03-28 21:12:13 +0100

    started working on combat report

---
+++ Code/Managers/LossesManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LossesManager : MonoBehaviour
+{
+
+    private int totalAlliedInfantryLosses;
+    private int totalAlliedTankLosses;
+    private int totalAlliedHorsemanLosses;
+    private int totalAlliedBattleRobotLosses;
+    private int totalEnemyInfantryLosses;
+
+    private int recentAlliedInfantryLosses;
+    private int recentAlliedTankLosses;
+    private int recentAlliedHorsemanLosses;
+    private int recentAlliedBattleRobotLosses;
+    private int recentEnemyInfantryLosses;
+
+    void Start()
+    {
+        
+    }
+
+}
