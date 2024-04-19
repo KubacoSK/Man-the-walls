8fb67f3fd6e9a8373d47d8c79709dd03c4ff6563
KubacoSK <jakublife123@gmail.com>
2024-04-19 23:27:29 +0200

    upgrade system

--- Code/Managers/UpgradeSystem.cs
+++ Code/Managers/UpgradeSystem.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class UpgradeSystem : MonoBehaviour
 {
+    public static UpgradeSystem Instance;
     bool CitizensIncrease;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one ResourceManager! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+    public void ActivateCitizensIncrease()
+    {
 
+    }
 }
