c2ed5b07c39fbb564500d1d959313b25cae877d8
KubacoSK <jakublife123@gmail.com>
2025-01-31 19:39:19 +0100

    added new sounds and stuff

--- Code/UI Scripts/MainMenu.cs
+++ Code/UI Scripts/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] AudioSource GearSound;
     public void QuitGame()
     {
         Application.Quit();
@@ -13,5 +14,6 @@ public class MainMenu : MonoBehaviour
     public void Play()
     {
         animator.SetTrigger("Play");
+        GearSound.Play();
     }
 }
