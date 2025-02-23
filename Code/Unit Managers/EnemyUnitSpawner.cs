d2de10c4a300ebf24e5ade861751d19337b28d93
KubacoSK <jakublife123@gmail.com>
2025-02-23 18:19:40 +0100

    added new unit

--- Code/Unit Managers/EnemyUnitSpawner.cs
+++ Code/Unit Managers/EnemyUnitSpawner.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class EnemyUnitSpawner : MonoBehaviour
 {
     [SerializeField] private Unit SpawnUnitTemplateEnemy;
+    [SerializeField] private Unit SpawnStrongerUnitTemplateEnemy;
     [SerializeField] private Zone[] EnemySpawnZones;
+    private int strongEnemySpawnerCount;
     public static EnemyUnitSpawner Instance;
     
     private void Awake()
@@ -17,12 +19,14 @@ public class EnemyUnitSpawner : MonoBehaviour
             return;
         }
         Instance = this;
+        strongEnemySpawnerCount = 2;
     }
     public void SpawnEnemyAtTurn()
     {
         List<Zone> zones = new List<Zone>();
         zones.Add(EnemySpawnZones[UnityEngine.Random.Range(0, 4)]);
         zones.Add(EnemySpawnZones[UnityEngine.Random.Range(0, 4)]);
+        if (strongEnemySpawnerCount <= 0) zones.Add(EnemySpawnZones[UnityEngine.Random.Range(0, 4)]);
         if (DifficultySetter.GetDifficulty() == "Hard" || DifficultySetter.GetDifficulty() == "Nightmare") zones.Add(EnemySpawnZones[UnityEngine.Random.Range(0, 4)]);
         foreach (Zone zone in zones)
         {
@@ -30,6 +34,14 @@ public class EnemyUnitSpawner : MonoBehaviour
             {
                 if (!zone.GetEnemyMoveLocationStatuses()[i])
                 {
+                    if (strongEnemySpawnerCount <= 0)
+                    {
+                        Vector3 strongSpawnPosition = zone.GetEnemyMoveLocations()[i];
+                        zone.SetEnemyPositionStatus(i, true);
+                        Unit strongUnit = Instantiate(SpawnStrongerUnitTemplateEnemy, strongSpawnPosition, Quaternion.identity);
+                        strongUnit.SetStandingZone(zone, i);
+                        strongEnemySpawnerCount = 2;
+                    }
                     Vector3 spawnPosition = zone.GetEnemyMoveLocations()[i];
                     zone.SetEnemyPositionStatus(i, true);
                     Unit unit = Instantiate(SpawnUnitTemplateEnemy, spawnPosition, Quaternion.identity);
@@ -39,5 +51,6 @@ public class EnemyUnitSpawner : MonoBehaviour
                 }
             }
         }
+        strongEnemySpawnerCount--;
     }
 }
